493d699d59d393e6707904e2e51140dcba8b0503
Aleks Tyc <aleks.tyc@gmail.com>
2026-01-06 22:04:21 +0100

    SimpleCounter UI tidy

--- UI/SimpleCounterToy/index.js
+++ UI/SimpleCounterToy/index.js
@@ -6,18 +6,9 @@ const btnIncrease = document.getElementById('inc-btn');
 const btnUpdate = document.getElementById('upd-btn');
 
 function increaseCounter() {
-    var boogieCount = 0;
-    console.log('* I * increaseCounter loop start');
-    for (let index = 10; index < 10000000; index++) {
-        if (Math.abs(index - Math.exp(Math.log(index))) < 0.1) {
-            boogieCount += index;
-        }
-    }
-    console.log('* I * increaseCounter loop end', boogieCount);
-    if (boogieCount === 9999999 * 10000000 / 2 -45) {
-        simpleCounter += 1;
-    }
+    simpleCounter += 1;
 }
+
 function getCounter() {
     return new Promise((resolve, reject) => { resolve(simpleCounter); });
 }
@@ -28,7 +19,6 @@ function clickUpdate() {
     promisedCounter.then(value => {
         counterDisplay.innerText = value;
         btnIncrease.disabled = false;
-        // btnIncrease.addEventListener("click", clickIncrease);
         btnIncrease.innerText = 'Increase';
     });
 }
@@ -36,7 +26,6 @@ function clickUpdate() {
 function clickIncrease() {
     btnIncrease.disabled = true;
     btnIncrease.innerText = 'Increasing...'
-    // btnIncrease.removeEventListener("click", clickIncrease);
 
     increaseCounter();
     clickUpdate();
@@ -48,4 +37,3 @@ btnUpdate.addEventListener("click", clickUpdate);
 btnUpdate.innerText = 'Update';
 
 clickUpdate();
-
