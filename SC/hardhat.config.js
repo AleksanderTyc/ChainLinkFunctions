604cf618fe083c5815088b7ca08f93ed90b0a40f
Aleks Tyc <aleks.tyc@gmail.com>
2026-01-16 17:25:20 +0100

    HardHat config do not compile specific .sol files

--- SC/hardhat.config.js
+++ SC/hardhat.config.js
@@ -1,5 +1,19 @@
 require("@nomicfoundation/hardhat-toolbox");
 
+const { TASK_COMPILE_SOLIDITY_GET_SOURCE_PATHS } = require("hardhat/builtin-tasks/task-names");
+const { subtask } = require("hardhat/config");
+
+subtask(TASK_COMPILE_SOLIDITY_GET_SOURCE_PATHS).setAction(
+  async (_, __, runSuper) => {
+    const paths = await runSuper();
+    // Filter out any undesired contracts, e.g. CLFunctionTemperatureInsurer.sol
+    return paths.filter(
+      p => !p.endsWith("CLFunctionTemperatureInsurer.sol")
+    );
+  }
+);
+// https://www.mexc.com/en-NG/news/90452
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
