6bf9b4cc8cc6d01bfdf5e083e233a79c44f70018
Aleks Tyc <aleks.tyc@gmail.com>
2026-01-19 22:19:32 +0100

    TemperatureInsurer SC some notes on verification on HardHat

--- SC/ignition/modules/TemperatureInsurer.js
+++ SC/ignition/modules/TemperatureInsurer.js
@@ -10,3 +10,20 @@ const TemperatureInsurerModule = buildModule("TemperatureInsurerModule", (m) => 
 });
 
 module.exports = TemperatureInsurerModule;
+
+/*
+const hre = require('hardhat');
+
+  // Verify on local network
+  hre.run("verify:verify", {
+    address: contractTemperatureInsurer.address,
+    constructorArguments: [272n * 10n ** 18n],
+  });
+ChainConfigNotFoundError: Trying to verify a contract in a network with chain id 31337, but the plugin doesn't recognize it as a supported chain.
+
+You can manually add support for it by following these instructions: https://v2.hardhat.org/verify-custom-networks
+
+To see the list of supported networks, run this command:
+
+  npx hardhat verify --list-networks
+*/
